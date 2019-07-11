64a6f42938ef069be60ac9d5d16770cca0e03d81
Jose Dieck <josedieck91@gmail.com>
2019-07-11 14:35:48 -0500

    Step 1 completed.

--- src/main/java/ShoppingCart.java
+++ src/main/java/ShoppingCart.java
@@ -1,4 +1,17 @@
 package main.java;
 
 public class ShoppingCart {
+
+    public static void checkout(String[] items) {
+        double total = 0;
+
+        for (int i = 0; i < items.length; i++) {
+            if(items[i].equalsIgnoreCase("apple")) {
+                total += 0.60;
+            } else if (items[i].equalsIgnoreCase("orange")) {
+                total += 0.25;
+            }
+        }
+        System.out.println("The total is: \u00A3" + String.format("%.2f", total));
+    }
 }
